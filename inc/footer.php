bc9d787a565e97785a181df499c1fd01801b47a6
leylaglasic <lejlaglasic8@gmail.com>
2023-09-21 23:56:56 +0200

    inc

---
+++ inc/footer.php
@@ -0,0 +1,17 @@
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+  <div class="col-md-4 d-flex align-items-center">
+    <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+      <svg class="bi" width="30" height="24">
+        <use xlink:href="#bootstrap" />
+      </svg>
+    </a>
+    <span class="mb-3 mb-md-0 text-body-secondary">&copy; 2023 by Lejla G</span>
+  </div>
+</footer>
+</div>
+<script src="/biblioteke/bootstrap-5.2.3-dist/js/bootstrap.bundle.min.js"></script>
+<!-- Custom styles for this template -->
+<link href="css/footer.css" rel="stylesheet">
+</body>
+
+</html>
